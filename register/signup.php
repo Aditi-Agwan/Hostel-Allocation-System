8163321aa74577f44afa1ac1e105d08f3f35d512
ridhed <ridhed2311@gmail.com>
2022-03-27 00:30:03 +0530

    login radio

--- register/signup.php
+++ register/signup.php
@@ -1,6 +1,6 @@
 <?php
-        //header.php
-        include('header.php');
+//header.php
+include('header.php');
 ?>
 
 
@@ -9,21 +9,21 @@
     <div class="row m-0">
         <div class="col-lg-4 offset-lg-2">
             <div class="text-center pb-5">
-            <h1 class="login-title text-dark">Signup</h1>
-            <p class="p-1 m-0 font-ubuntu text-black-50">Signup for Hostel Allocation Process</p>
-            <span class="font-ubuntu text-black-50">I already have <a href="login.php">Login</a></span>
+                <h1 class="login-title text-dark">Signup</h1>
+                <p class="p-1 m-0 font-ubuntu text-black-50">Signup for Hostel Allocation Process</p>
+                <span class="font-ubuntu text-black-50">I already have <a href="login.php">Login</a></span>
             </div>
             <div class="d-flex justify-content-center">
                 <form action="signup.php" method="post" enctype="multipart/form-data" id="sign-form">
                     <div class="form-row">
-                        <div class="col">
-                            <input type="text" name="firstName" id="firstName" class="form-control" placeholder="First Name"> 
+                        <div class=".col-sm-4">
+                            <input type="text" name="firstName" id="firstName" class="form-control" placeholder="First Name">
                         </div>
                         <div class="col">
-                            <input type="text" name="middleName" id="middleName" class="form-control" placeholder="Middle Name"> 
+                            <input type="text" name="middleName" id="middleName" class="form-control" placeholder="Middle Name">
                         </div>
                         <div class="col">
-                            <input type="text" name="lastName" id="lastName" class="form-control" placeholder="Last Name"> 
+                            <input type="text" name="lastName" id="lastName" class="form-control" placeholder="Last Name">
                         </div>
                     </div>
                     <div class="form-row my-4">
@@ -41,6 +41,19 @@
                             <input type="password" required name="confirm_pwd" id="confirm_pwd" class="form-control" placeholder="Confirm Password*">
                         </div>
                     </div>
+                    <div class="form-row">
+                        <div class="col">
+                            <label>Gender</label><br>
+                        </div>
+                        <div class="col">
+                            <input type="radio" name="gender" value="Male">Male<br>
+                        </div>
+                        <div class="col">
+                            <input type="radio" name="gender" value="Female">Female<br>
+                        </div>
+                        <input type="submit" name="Signup" value="Signup">
+
+                    </div>
                 </form>
             </div>
 
@@ -51,6 +64,6 @@
 <!-------Signup Area-------->
 
 <?php
-        //footer.php
-        include('footer.php');
+//footer.php
+include('footer.php');
 ?>
