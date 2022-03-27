65c2f23e4765581a7d5716386e227cbb59a70c66
ridhed <ridhed2311@gmail.com>
2022-03-27 10:27:34 +0530

    commit login changes

--- register/login.php
+++ register/login.php
@@ -10,7 +10,7 @@ include('header.php');
         <div class="col-lg-4 offset-lg-2">
             <div class="text-center pb-5">
                 <h1 class="login-title text-dark">Login</h1>
-                <p class="p-1 m-0 font-ubuntu text-black-50">Signup for Hostel Allocation Process</p>
+                <p class="p-1 m-0 font-ubuntu text-black-50">Login for Hostel Allocation Process</p>
                 <span class="font-ubuntu text-black-50">I already have <a href="login.php">Login</a></span>
             </div>
             <div class="d-flex justify-content-center">
@@ -25,11 +25,6 @@ include('header.php');
                             <input type="password" required name="password" id="passsword" class="form-control" placeholder="Password*">
                         </div>
                     </div>
-                    <div class="form-row my-4">
-                        <div class="col">
-                            <input type="password" required name="confirm_pwd" id="confirm_pwd" class="form-control" placeholder="Confirm Password*">
-                        </div>
-                    </div>
                 </form>
             </div>
 
@@ -37,7 +32,7 @@ include('header.php');
     </div>
 </section>
 
-<!-------Signup Area-------->
+<!-------Login Area-------->
 
 <?php
 //footer.php
